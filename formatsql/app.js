d45d48d961210bd93f6b28a52fe38d87f5189ef2
DucHo <hohuuduc2709@gmail.com>
2024-08-27 00:54:19 +0700

    Format SQL (webassembly)

---
+++ formatsql/app.js
@@ -0,0 +1,6 @@
+var init = function () {
+    // const duc = DotNet.invokeMethod("FormatSQL", "Parse", "Duc");
+    // console.log(duc);
+};
+Blazor.start().then(init.bind(this));
+//# sourceMappingURL=app.js.map
